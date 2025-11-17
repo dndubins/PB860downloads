f278928447326f55a17c270e54b514776a0cec44
David Dubins <d.dubins@utoronto.ca>
2025-11-17 16:07:42 -0500

    Add files via upload

---
+++ pHmeter.ino
@@ -0,0 +1,40 @@
+// pHmeter.ino
+// Bail-Out program for Activity 7-2
+// Author: D. Dubins
+// Date: 17-Nov-25
+// Libraries required: None
+// Wiring:
+// -------
+// Uno - phAmplifier
+// GND - GND
+// +5V - V+
+// A1 - Vout
+//
+// Note:
+// Connect MCU AREF pin to 3.3V.
+
+byte pHPin = A1;                  // Declaring the Analog input to be Pin A2 of Arduino board.
+float probeSlope = -5.333333333;  // Ideal slope, dpH/dVolts, should be negative
+float probeInt = 16.14933333;     // Ideal intercept (in pH units)
+
+void setup() {
+  Serial.begin(9600);         // Start the Serial Monitor
+  analogReference(EXTERNAL);  // use AREF for reference voltage. Run the 3.3V out to the Aref pin on the Arduino.
+  Serial.println("");         // Results in less first-line garbage when printing to Serial monitor
+  Serial.println("pH to Serial"); // Print header
+  Serial.println("------------");
+  Serial.println("Divs (0-1023), Volts (V), pH");
+  Serial.println("");
+}
+
+void loop() {
+  int div = analogRead(pHPin);               // get divs
+  float volts = div * 3.3 / 1023.0;          // get divs and convert to volts
+  float pH = volts * probeSlope + probeInt;  // convert volts to pH
+  Serial.print(div);                         // print to serial monitor
+  Serial.print(",  ");
+  Serial.print(volts, 3);  // print to serial monitor
+  Serial.print(",  ");
+  Serial.println(pH, 1);
+  delay(250);
+}
---
+++ scale.ino
@@ -0,0 +1,56 @@
+// scale.ino
+// Bail-Out program for Activity 7-1
+// Author: D. Dubins
+// Date: 17-Nov-25
+// Libraries required: None
+// Wiring:
+// -------
+// Uno - loadCellAmp
+// GND - GND
+// +5V - Vcc
+// Vout - A1
+// SW - D4
+//
+// Note:
+// Connect MCU AREF pin to 3.3V.
+
+#define NUMREADS 100  // number of readings for data smoothing
+int weightPin = A1;
+int buttonPin = 4;
+
+float scaleSlope = 378.07;  // call weight
+float scaleInt = -210.96;   // div intercept when scale is empty
+
+float weight = 0.0;
+float tareWeight = 0.0;
+\
+void setup() {
+  analogReference(EXTERNAL);         // use external analog reference voltage of 3.3V
+  pinMode(buttonPin, INPUT_PULLUP);  // Set up pin 4 as a button. Watch the logic here.
+  Serial.begin(9600);                // Opens serial port, sets data rate to 9600 bps
+  Serial.println("");
+  Serial.println("Weight to Serial Output");  // Print header text to Serial monitor
+  Serial.println("-----------------------");
+  Serial.println("Divs (0-1023), Volts (V), Weight (g)");
+  Serial.println("");
+}
+
+void loop() {
+  int divs = analogRead(weightPin);                         // take a reading in divs
+  float voltage = divs * 3.3 / 1023.0;                      // convert to volts
+  weight = scaleInt + (scaleSlope * voltage) - tareWeight;  // calculate weight
+  if (digitalRead(buttonPin) == LOW) {                      // if button is pushed
+    Serial.println("**ZEROING***");                         // zero the scale
+    delay(2000);                                            // debounce (finger pressed skews reading)
+    divs = analogRead(weightPin);                           // take a reading in divs
+    voltage = divs * 3.3 / 1023.0;                          // convert to volts
+    weight = scaleInt + (scaleSlope * voltage);             // convert to weight
+    tareWeight = weight;                                    // store weight to tareWeight, to get subtracted
+  }
+  Serial.print(divs);  // print divs to Serial Monitor
+  Serial.print(", ");
+  Serial.print(voltage, 3);  // print volts to Serial Monitor
+  Serial.print(", ");
+  Serial.println(weight, 1);  // print weight to Serial Monitor
+  delay(100);
+}  // End loop
